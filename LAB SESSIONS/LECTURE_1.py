f6896de1449e9c3e163850956c4aeb8822bc9ea3
Miriam Bonello <bonellomiriam@gmail.com>
2022-11-14 22:35:52 +0100

    SESSION 1 DONE

--- LAB SESSIONS/LECTURE_1.py
+++ LAB SESSIONS/LECTURE_1.py
@@ -3,109 +3,108 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# creating the empty lists to be used
-xlist = []
-ylist = []
-prodlist = []
-denomlist = []
-ydenomlist = []
-yelist = []
+#empty lists to be used later on 
+x_list = []
+y_list = []
+prod_list = []
+denom_list = []
+ydenom_list = []
+ye_list = []
 
-# reading the data
+#to be able to read the data 
 data = pd.read_csv('Q1__Youngs_Modulus_of_a_Wire.csv')
-# changing the individual columns into arrays
-diameter_array = (data['Diameter/m']).to_numpy()
-mass_array = data['m/kg'].to_numpy()
-x1_array = data['x_1/m'].to_numpy()
-x2_array = data['x_2/m'].to_numpy()
-x3_array = data['x_3/m'].to_numpy()
-x4_array = data['x_4/m'].to_numpy()
-L_array = data['L/m'].to_numpy()
-x0_array = data['x_0/m'].to_numpy()
+#setting the columns into numpy arrays 
+diameter = (data['Diameter/m']).to_numpy()
+mass = data['m/kg'].to_numpy()
+x0 = data['x_0/m'].to_numpy()
+x1 = data['x_1/m'].to_numpy()
+x2 = data['x_2/m'].to_numpy()
+x3 = data['x_3/m'].to_numpy()
+x4 = data['x_4/m'].to_numpy()
+L = data['L/m'].to_numpy()
 
-# finding the x-x0 part of the equation
-for i in range(len(x1_array)-1):
-    x = sum([x1_array[i], x2_array[i], x3_array[i], x4_array[i]])
-    X = (x/4)-x0_array[0]
-    xlist.append(abs(X))
+#calculating x-x0
+for i in range(len(x1)-1):
+    x = sum([x1[i], x2[i], x3[i], x4[i]])
+    X = (x/4)-x0[0]
+    x_list.append(abs(X))
 
-# calculating xi and xbar
-xi = np.array(xlist)**2
-xbar = np.mean(xi)
-print(xbar)
+#calculating x_i and x_bar
+x_i = np.array(x_list)**2
+x_bar = np.mean(x_i)
+print(x_bar)
 
-# calculating yi and y bar
-for i in range(len(mass_array)-1):
-    yi = mass_array[i]/xlist[i]
-    ylist.append(yi)
+#calculating yi and y bar
+for i in range(len(mass)-1):
+    y_i = mass[i]/x_list[i]
+    y_list.append(y_i)
 
-yi = np.array(ylist)
-ybar = np.mean(yi)
-print(ybar)
+y_i = np.array(y_list)
+y_bar = np.mean(y_i)
+print(y_bar)
 
-# defining the function to calculate alpha, beta, coefficient of correlation and determination
-def beta_alpha_function(xi, xbar, yi, ybar):
-    for i in range(len(xi)):
-        prod = (xi[i]-xbar)*(yi[i]-ybar)
-        prodlist.append(prod)
-        xdenom = (xi[i]-xbar)**2
-        denomlist.append(xdenom)
-        ydenom = (yi[i]-ybar)**2
-        ydenomlist.append(ydenom)
-    prod_array = np.array(prodlist)
-    denom_array = np.array(denomlist)
-    ydenom_array = np.array(ydenomlist)
-    numerator = sum(prod_array)
-    denominator = sum(denom_array)
-    ydenominator = sum(ydenom_array)
+#creating a function to be able to calculate the alpha function, the beta and the coefficient of correlation along with the determination 
+def beta_alpha_function(x_i, x_bar, y_i, y_bar):
+    for i in range(len(x_i)):
+        prod = (x_i[i]-x_bar)*(y_i[i]-y_bar)
+        prod_list.append(prod)
+        xdenom = (x_i[i]-x_bar)**2
+        denom_list.append(xdenom)
+        ydenom = (y_i[i]-y_bar)**2
+        ydenom_list.append(ydenom)
+    prodarray = np.array(prod_list)
+    denomarray = np.array(denom_list)
+    ydenomarray = np.array(ydenom_list)
+    numerator = sum(prodarray)
+    denominator = sum(denomarray)
+    ydenominator = sum(ydenomarray)
     beta = numerator/denominator
-    alpha = ybar - (beta*xbar)
+    alpha = y_bar - (beta*x_bar)
     r = numerator/(np.sqrt(denominator*ydenominator))
-    delta_beta = (beta/(np.sqrt(len(xi)-2)))*(np.sqrt((1/r**2)-1))
-    delta_alpha = delta_beta*np.sqrt(((1/len(xi))*(sum(xi**2))))
+    delta_beta = (beta/(np.sqrt(len(x_i)-2)))*(np.sqrt((1/r**2)-1))
+    delta_alpha = delta_beta*np.sqrt(((1/len(x_i))*(sum(x_i**2))))
     R = r**2
     return beta, alpha, delta_beta, delta_alpha, r, R
 
-# calling previous function to get the actual values
-beta, alpha, delta_beta, delta_alpha, r, R = beta_alpha_function(xi, xbar, yi, ybar)
+#to obtain the actual values 
+beta, alpha, delta_beta, delta_alpha, r, R = beta_alpha_function(x_i, x_bar, y_i, y_bar)
 
-# calculating the experimental values 
-for i in range(len(xi)):
-    ye = alpha + (beta*xi[i])
-    yelist.append(ye)
-ye_array = np.array(yelist)
+#to obtain a value for the experimental value 
+for i in range(len(x_i)):
+    ye = alpha + (beta*x_i[i])
+    ye_list.append(ye)
+ye_array = np.array(ye_list)
 
-# calculating the constants for the gradient and intercept
-radius = np.average(diameter_array)
-m_constant = (8*np.pi*(radius**2))/(9.81*(L_array[0]**3))
-c_constant = 4/(L_array[0]*9.81)
+#to obtain the concepts for the intercept and gradient 
+radius = np.average(diameter)
+m_constant = (8*np.pi*(radius**2))/(9.81*(L[0]**3))
+c_constant = 4/(L[0]*9.81)
 
-# finding the line of best fit to the data
-coeffs, cov = np.polyfit(xi, ye_array, 1, cov=True)
+#obtaining the best fit line 
+coeffs, cov = np.polyfit(x_i, ye_array, 1, cov=True)
 polyfunc = np.poly1d(coeffs)
-trendline = polyfunc(xi)
+trendline = polyfunc(x_i)
 
-# calculating the young's modulus and T0
+#obtaing Young's Modulus and T0
 E = coeffs[0]/m_constant
 T0 = coeffs[1]/c_constant
 delta_E = np.sqrt(cov[0][0])
 delta_T0 = np.sqrt(cov[1][1])
 print(E, delta_E, T0, delta_T0)
 
-# calculating the residuals
-residual = np.subtract(yi,trendline)
+#obtaining a value for the residual 
+residual = np.subtract(y_i,trendline)
 
 f, (a0, a1) = plt.subplots(2, 1, sharex=True, sharey=False, gridspec_kw={'height_ratios': [3, 1]}, figsize=(7.3, 10.7))
 
-# defining the font to be used
+#setting the font for the graph and setting the paraments to obtain the graphsa s one subplot 
 plt.rcParams['font.family'] = 'STIXGeneral'
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['font.size'] = 12
 plt.rcParams['font.weight'] = 'normal'
 
-# plotting both graph as subplots
-a0.scatter(xi, yi, color='k', label='Data Points')
-a0.plot(xi, trendline, color='k', label='Trendline')
+a0.scatter(x_i, y_i, color='k', label='Data Points')
+a0.plot(x_i, trendline, color='k', label='Trendline')
 a0.minorticks_on()
 a0.grid(visible=True, which='major', linestyle='-')
 a0.grid(visible=True, which='minor', linestyle='--')
